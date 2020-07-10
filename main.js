a4d66c662d416c0cd70d0238cbdadf4e8bd437aa
mayowa <smayowa176gmail.com>
2020-07-10 14:58:09 +0100

    added whatsap icon made the links open new tab

--- main.js
+++ main.js
@@ -12,11 +12,12 @@ function changeTheme(){
         document.querySelector("#button").style.background = "black";
         document.querySelector("#button").style.color = "#05B993";
         document.querySelector("#button").style.border = "none";
-        document.querySelector(" .fa-facebook-square").classList.add("dark-fa")
-        document.querySelector(" .fa-twitter").classList.add("dark-fa")
-        document.querySelector(" .fa-instagram").classList.add("dark-fa")
-        document.querySelector(" .fa-github").classList.add("dark-fa")
-        document.querySelector(" .fa-linkedin").classList.add("dark-fa")
+        document.querySelector(" .fa-facebook-square").classList.add("dark-fa");
+        document.querySelector(" .fa-twitter").classList.add("dark-fa");
+        document.querySelector(" .fa-instagram").classList.add("dark-fa");
+        document.querySelector(" .fa-github").classList.add("dark-fa");
+        document.querySelector(" .fa-linkedin").classList.add("dark-fa");
+        document.querySelector(" .fa-whatsapp").classList.add("dark-fa");
     }else{
         document.body.style.background= "white"
         document.querySelector(".left-box").classList.remove("dark-left-box")
@@ -24,13 +25,14 @@ function changeTheme(){
         document.querySelector("#button").style.background = "white";
         document.querySelector("#button").style.color = "black";
         document.querySelector("#button").style.border = "none";
-        document.querySelector(".left-box").classList.remove("dark-left-box")
-        document.querySelector("img").classList.remove("dark-img")
-        document.querySelector("#create").classList.remove("dark-create")
-        document.querySelector(" .fa-facebook-square").classList.remove("dark-fa")
-        document.querySelector(" .fa-twitter").classList.remove("dark-fa")
-        document.querySelector(" .fa-instagram").classList.remove("dark-fa")
-        document.querySelector(" .fa-github").classList.remove("dark-fa")
-        document.querySelector(" .fa-linkedin").classList.remove("dark-fa")
+        document.querySelector(".left-box").classList.remove("dark-left-box");
+        document.querySelector("img").classList.remove("dark-img");
+        document.querySelector("#create").classList.remove("dark-create");
+        document.querySelector(" .fa-facebook-square").classList.remove("dark-fa");
+        document.querySelector(" .fa-twitter").classList.remove("dark-fa");
+        document.querySelector(" .fa-instagram").classList.remove("dark-fa");
+        document.querySelector(" .fa-github").classList.remove("dark-fa");
+        document.querySelector(" .fa-linkedin").classList.remove("dark-fa");
+        document.querySelector(" .fa-whatsapp").classList.remove("dark-fa"); 
     }
 }
